e0146d7f8121344cf9aa6b32ce9a1351c52c6ee9
tcchoudhary <tcchoudhary@gmail.com>
2025-08-13 23:42:47 +0530

    db change

--- apps/config/database.js
+++ apps/config/database.js
@@ -2,14 +2,10 @@ const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize({
   dialect: "mysql",
-  host: process.env.host,
-  username:process.env.username,
-  password: process.env.password,
-  database: process.env.database,
-  // host:"localhost",
-  // username:"root",
-  // // password:"",
-  // database:"sql_practice_db",
+  host: "dashaboard.jaladhija.com",
+  username:"u777337353_jaladhija",
+  password: "Dafesgdrh5434534@#$%",
+  database:"u777337353_jaladhija_dash",
   logging: false,
 });
 
