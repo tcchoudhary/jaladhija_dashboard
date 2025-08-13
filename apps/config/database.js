37995259dc3aac8cc90865a77dc5fc59eab6d867
tcchoudhary <tcchoudhary@gmail.com>
2025-08-13 23:36:42 +0530

    port change 3000

--- apps/config/database.js
+++ apps/config/database.js
@@ -2,14 +2,14 @@ const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize({
   dialect: "mysql",
-  // host: process.env.host,
-  // username:process.env.username,
-  // password: process.env.password,
-  // database: process.env.database,
-  host:"localhost",
-  username:"root",
-  // password:"",
-  database:"sql_practice_db",
+  host: process.env.host,
+  username:process.env.username,
+  password: process.env.password,
+  database: process.env.database,
+  // host:"localhost",
+  // username:"root",
+  // // password:"",
+  // database:"sql_practice_db",
   logging: false,
 });
 
